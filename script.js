4cae762f3c2efbfb7332397d1953e276ff13af82
Moisés <moirs@proton.me>
2025-06-26 09:19:12 -0600

    Hamburger menu bug

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ Iconsearch.addEventListener("click", () => {
   let inputSearch = document.querySelector("#search");
   inputSearch.classList.toggle("block");
   inputSearch.classList.toggle("translate");
-
-  // if it’s now visible, give it focus
   if (inputSearch.classList.contains("block")) {
     inputSearch.focus();
   }
 });
-
